ae12e092f9be693babcc3dbef7b7d731f50b1745
772 <>
2025-10-11 12:06:56 +0200

    Update.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    single_variable_algebra_compiler::read_input();
+    output("TEST".to_string());
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn output(s: String) {
+    println!("{s}");
+}
+
+#[cfg(target_arch = "wasm32")]
+fn output(s: String) {
+    use web_sys::wasm_bindgen::JsCast;
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let textarea = document
+        .get_element_by_id("input")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlTextAreaElement>()
+        .unwrap();
+    textarea.set_value(&s);
 }
